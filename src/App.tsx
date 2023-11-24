e695ceccd35801c8d515f44101b4fc0daf8cb29a
Lachicagladiadora <200379@unsaac.edu.pe>
2023-11-23 20:45:42 -0500

    fix(app):view last item

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,50 @@
 import "./App.css";
 import { useEffect, useState } from "react";
-import { ENGLISH_ADVANCED_VOCABULARY_LIST, ENGLISH_BASIC_VOCABULARY_LIST, ENGLISH_INTERMEDIATE_VOCABULARY_LIST, NOTE_MUSICAL_LIST } from "./constants";
+import {
+  ENGLISH_ADVANCED_VOCABULARY_LIST,
+  ENGLISH_BASIC_VOCABULARY_LIST,
+  ENGLISH_INTERMEDIATE_VOCABULARY_LIST,
+  NOTE_MUSICAL_LIST,
+} from "./constants";
 import { Card } from "./components/Card";
 import { ListsSelector, Lists } from "./components/ListsSelector";
 import { Button } from "./components/Button";
 
 export type SelectedList = {
-  label: string,
-  value: string
-}
+  label: string;
+  value: string;
+};
 
-const LISTS: Lists = { noteMusical: { value: NOTE_MUSICAL_LIST, label: 'Note Musical' }, englishBasicVocabulary: { value: ENGLISH_BASIC_VOCABULARY_LIST, label: 'English Basic Vocabulary' }, englishIntermediateVocabulary: { value: ENGLISH_INTERMEDIATE_VOCABULARY_LIST, label: 'English Intermediate Vocabulary' }, englishAdvancedVocabulary: { value: ENGLISH_ADVANCED_VOCABULARY_LIST, label: 'English Advanced Vocabulary' } }
+const LISTS: Lists = {
+  noteMusical: { value: NOTE_MUSICAL_LIST, label: "Note Musical" },
+  englishBasicVocabulary: {
+    value: ENGLISH_BASIC_VOCABULARY_LIST,
+    label: "English Basic Vocabulary",
+  },
+  englishIntermediateVocabulary: {
+    value: ENGLISH_INTERMEDIATE_VOCABULARY_LIST,
+    label: "English Intermediate Vocabulary",
+  },
+  englishAdvancedVocabulary: {
+    value: ENGLISH_ADVANCED_VOCABULARY_LIST,
+    label: "English Advanced Vocabulary",
+  },
+};
 
 const randomValue = (values: string[]): string => {
-  const randomIdx = Math.floor(Math.random() * (values.length - 1));
+  const randomIdx = Math.floor(Math.random() * values.length);
   return values[randomIdx];
 };
 
 function App() {
   /**
-   * TODO: 
-   * - create selectedList state with type <{{label:string,value:string}}> 
+   * TODO:
+   * - create selectedList state with type <{{label:string,value:string}}>
    * - create a object lists = { nameOfList1: [...hereDestructureTheListFromConstant], nameOfList2:[], ...}
    * - add the click event on the <option/> Html element to change the selectedList
    * - use the selected list to generate a the random value
    * - create a PR
-   * 
+   *
    * TODO:
    * - discommend the add and edit button from AllLists component
    * - create a state displayListForm
@@ -39,82 +58,93 @@ function App() {
    */
   const [randomCard, setRandomCard] = useState("");
   const [isPlaying, setIsPlaying] = useState(false);
-  const [selectedList, setSelectedList] = useState<SelectedList | null>({label: 'Note Musical',value:'noteMusical'});
-  const [isDarkMode, setIsDarkMode] = useState(false)
+  const [selectedList, setSelectedList] = useState<SelectedList | null>({
+    label: "Note Musical",
+    value: "noteMusical",
+  });
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
   useEffect(() => {
     if (!isPlaying) return;
     const intervalId = setInterval(() => {
-      if (!selectedList) return console.log('not exist list')
-     
-      setRandomCard(randomValue(LISTS[selectedList.value].value))
+      if (!selectedList) return console.log("not exist list");
+
+      setRandomCard(randomValue(LISTS[selectedList.value].value));
     }, 1000);
     return () => clearInterval(intervalId);
   }, [isPlaying]);
 
-  const backgroundColor =  isDarkMode ? 'rgb(206,206,206)' : "#1E1E1E"
-  const color = isDarkMode  ? "#1E1E1E" : 'rgb(206,206,206)' 
-  const backgroundCard = isDarkMode ? '#FBFBFB' : '#6C6C6C'
-
+  const backgroundColor = isDarkMode ? "rgb(206,206,206)" : "#1E1E1E";
+  const color = isDarkMode ? "#1E1E1E" : "rgb(206,206,206)";
+  const backgroundCard = isDarkMode ? "#FBFBFB" : "#6C6C6C";
 
   return (
     <div
       style={{
-        position:"absolute",
-        left:"0px",
-        top:"0px",
-        right:"0px",
-        bottom:"0px",
-        height: '100%',
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        gap: '30px',
+        position: "absolute",
+        left: "0px",
+        top: "0px",
+        right: "0px",
+        bottom: "0px",
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "30px",
         background: backgroundColor,
         color: color,
       }}
     >
-      {
-        selectedList &&
+      {selectedList && (
         <>
           <Card
             value={randomCard}
             backgroundColor={backgroundCard}
             color={color}
           />
-          <Button onclick={()=>setIsPlaying(!isPlaying)} theme={isDarkMode ? 'dark' : 'light'}>
+          <Button
+            onclick={() => setIsPlaying(!isPlaying)}
+            theme={isDarkMode ? "dark" : "light"}
+          >
             {isPlaying ? "▣ Stop" : "▶ Play"}
           </Button>
         </>
-      }
+      )}
 
       {!selectedList && <p>Select a list please</p>}
-      <ListsSelector lists={LISTS}  value={selectedList} onChange={setSelectedList} backgroundColor= {backgroundColor} color={color}/>
+      <ListsSelector
+        lists={LISTS}
+        value={selectedList}
+        onChange={setSelectedList}
+        backgroundColor={backgroundColor}
+        color={color}
+      />
 
-      <Button 
-        onclick={()=>setIsDarkMode(visual=>!visual)}
-        theme={isDarkMode ? 'dark' : 'light'}
+      <Button
+        onclick={() => setIsDarkMode((visual) => !visual)}
+        theme={isDarkMode ? "dark" : "light"}
         style={{
           fontSize: "50px",
-          position: 'absolute',
-          bottom: '40px',
-          right: '40px'
+          position: "absolute",
+          bottom: "40px",
+          right: "40px",
         }}
       >
-      {isDarkMode === true ? "☾" : "☀"}
+        {isDarkMode === true ? "☾" : "☀"}
       </Button>
 
       <p
         style={{
-          position: 'absolute',
-          bottom: '10px',
-          fontSize: '14px'
+          position: "absolute",
+          bottom: "10px",
+          fontSize: "14px",
         }}
-      >By elVengador & Lachicagladiadora - 2023</p>
+      >
+        By elVengador & Lachicagladiadora - 2023
+      </p>
     </div>
-
   );
 }
 
